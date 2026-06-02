9cb55dbc4fe4a8782f51b276e47a0deb3a4bfbe2
wzsgfr <2818747562@qq,com>
2026-06-02 16:15:17 +0800

    新增员工

--- sky-common/src/main/java/com/sky/exception/PasswordErrorException.java
+++ sky-common/src/main/java/com/sky/exception/PasswordErrorException.java
@@ -6,6 +6,7 @@ package com.sky.exception;
 public class PasswordErrorException extends BaseException {
 
     public PasswordErrorException() {
+
     }
 
     public PasswordErrorException(String msg) {
